61c36b5b6e82cf26662df87044216f5a7337ac64
aslamhus <aslam.percy.husain@gmail.com>
2023-03-05 11:43:54 -0800

    adjust SpeechBubble position according to constraints

--- src/Components/SpeechBubble/SpeechBubble.js
+++ src/Components/SpeechBubble/SpeechBubble.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState, useRef, useContext } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import gsap from 'gsap';
-import { GameStageContext } from '../GameStage/context';
+import { useGameStage } from '../GameStage/hooks/useGameStage';
+import { constraints } from '../GameStage/constraints';
 import './speech-bubble.css';
 
 let speechResolve;
@@ -10,7 +11,6 @@ export default function SpeechBubble({
   isVisible,
   style,
   anchor,
-  constraints,
   offset = { x: 0, y: 0 },
   // arrowPosition,
   showAnimationDuration = 0.5,
@@ -19,12 +19,17 @@ export default function SpeechBubble({
 }) {
   const [bubbleText, setBubbleText] = useState('');
   const [_show, setShow] = useState(false);
+  const [arrowPosition, setArrowPosition] = useState('left');
   const [positions, setPositions] = useState({
-    arrow: 'left',
+    top: '',
+    bottom: '',
     left: '',
     right: '',
   });
   const bubbleRef = useRef();
+
+  const { doesElementBreakConstraints, getComputedConstraints, getPositionRelativeToGameStage } =
+    useGameStage();
 
   const findArrowPosition = (left) => {
     return 'left';
@@ -39,10 +44,17 @@ export default function SpeechBubble({
   const setSpeechBubblePosition = () => {
     if (!anchor) return;
     const { left, top } = anchor.getBoundingClientRect();
+    // setPositions({
+    //   ...positions,
+    //   arrow: findArrowPosition(left),
+    //   left: left + offset.x,
+    //   top: top + offset.y,
+    // });
+    setArrowPosition(findArrowPosition(left));
     setPositions({
-      arrow: findArrowPosition(left),
-      left: left + offset.x,
-      top: top + offset.y,
+      ...positions,
+      left: '200px',
+      top: '20px',
     });
   };
 
@@ -58,8 +70,18 @@ export default function SpeechBubble({
      *
      *
      */
-    console.log('currentBubbleBounds', bubbleContainerBounds);
-    console.log('currentConstraints', constraints);
+    const breakConstraints = doesElementBreakConstraints(bubbleRef.current);
+    if (breakConstraints) {
+      const { constraintsBroken, constraintBounds, elementBounds } = breakConstraints;
+      console.error('speech bubble is outside of gamestage constraints', constraintsBroken);
+      const style = constraintsBroken.reduce((acc, constraint) => {
+        acc[constraint] = `${constraintBounds[constraint]}px`;
+        acc[constraints[constraint].opposite] = 'unset';
+        return acc;
+      }, {});
+      console.log('--------------');
+      setPositions({ ...style });
+    }
   };
 
   const parseSpeech = (speech) => ({ __html: speech });
@@ -77,29 +99,31 @@ export default function SpeechBubble({
       onBeforeShowSpeechBubble(bubbleRef?.current);
     }
     // constrain
+    // set scale to 1 before applying onstraints so we can calculate actual size
+    await gsap.set(bubbleRef.current, { scale: 1 });
     applyConstraints();
     setShow(true);
+    gsap.set(bubbleRef.current, { scale: 0 });
     await gsap.to(bubbleRef.current, { opacity: 1, scale: 1, duration: showAnimationDuration });
     if (onShowSpeechBubble instanceof Function) {
       onShowSpeechBubble(bubbleRef?.current);
     }
   };
 
-  const showBubbleSpeech = async () => {
+  const prepareSpeechBubble = async () => {
     if (!bubbleRef.current) {
       throw new Error('Speech bubble has not rendered');
     }
     if (show) {
       await hideBubble();
     }
-
+    // setting bubble text triggers showBubble
     setBubbleText(speech);
-    showBubble();
   };
 
   const getArrowLeftPosition = () => {
     let left = '';
-    switch (positions.arrow) {
+    switch (arrowPosition) {
       case 'right':
         left = '65%';
         break;
@@ -117,9 +141,21 @@ export default function SpeechBubble({
    */
   useEffect(() => {
     if (speech) {
-      showBubbleSpeech();
+      console.log('new speech set', speech);
+      prepareSpeechBubble();
     }
   }, [speech]);
+
+  /**
+   * Show the speech bubble
+   * (speech bubble is now hidden with its speech value set)
+   */
+  useEffect(() => {
+    if (bubbleText) {
+      console.log('speech set, show bubble now');
+      showBubble();
+    }
+  }, [bubbleText]);
 
   /**
    * Parent show differs from internal show state
@@ -131,15 +167,12 @@ export default function SpeechBubble({
   }, [show]);
 
   return (
-    <div
-      className="speech-bubble-container"
-      style={{ ...style, left: positions.left, top: positions.top }}
-    >
+    <div className="speech-bubble-container" style={{ ...style, ...positions }}>
       <div
         ref={bubbleRef}
         className="speech-bubble"
         style={{
-          transform: 'scale(0)',
+          transform: 'scale(1)',
           visibility: _show ? 'visible' : 'hidden',
         }}
       >
